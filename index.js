1a5b43a7d46c990ebd2b966563114aa64f4bcacf
Benny <asshimbenny01@gmail.com>
2026-04-08 09:11:57 +0300

    2nd push

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 // Function to change the background color when a button is clicked
 function changeBackgroundColor() {
   // Implement the function to change background color
-  document.body.style.backgroundColor = 'lightblue'
+  document.body.style.backgroundColor = ''
 }
 
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
-  document.body.style.backgroundColor = 'grey'
+  document.body.style.backgroundColor = 'white'
 }
 
 // Capture Keyboard Input
@@ -19,7 +19,7 @@ function displayKeyPress(event) {
   // Implement the function to display key pressed
   const keyPressed = event.key
   const displayElement = document.getElementById('keyPressDisplay')
-  displayElement.textContent = `Key Pressed: ${keyPressed}`
+  displayElement.textContent = `Key pressed: ${keyPressed}`
 }
 
 // Process Text Input
