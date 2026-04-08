2102f5f5456d27c9e481cd5fcc0e28105c38c504
Benny <asshimbenny01@gmail.com>
2026-04-08 09:23:31 +0300

    2nd push

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function changeBackgroundColor() {
 // Function to reset the background color when the body is double-clicked
 function resetBackgroundColor() {
   // Implement the function to reset background color
-  document.body.style.backgroundColor = 'white'
+  document.body.style.backgroundColor = ''
 }
 
 // Capture Keyboard Input
